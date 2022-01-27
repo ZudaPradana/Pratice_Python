df0385f7247d022eb4f39317872a886923a481df
callmezydd <31976928+ZudaPradana@users.noreply.github.com>
2022-01-27 21:30:41 +0700

    Update Fizzbuzz .py

--- Fizzbuzz .py
+++ Fizzbuzz .py
@@ -1,4 +1,4 @@
-# Challange FizzBuzz
+# Fizzbuzz using for looping
 for x in range(1,(100)+1):
     if x%3 == 0 and x%5 == 0:
         x = 'FizzBuzz'
@@ -7,18 +7,3 @@ for x in range(1,(100)+1):
     elif x%5 == 0:
         x = 'Buzz'
     print(x)
-
-
-# Fizzbuzz using while loop
-max_number = int(input('Maksimal angka: \n'))
-x = 0
-while x < max_number:
-    x += 1
-    if x % 3 == 0 and x % 5 == 0:
-        print(f'{x} is FizzBuzz')
-    elif x % 3 == 0:
-        print(f'{x} is Fizz')
-    elif x % 5 == 0:
-        print(f'{x} is Buzz')
-    else:
-        print(x)
